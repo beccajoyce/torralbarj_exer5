137f2312a52940982291073ba4805f4c254bc880
beccajoyce <rrtorralba@up.edu.ph>
2024-03-13 18:15:53 +0800

    feat:added post method for book and get method for author

--- request.js
+++ request.js
@@ -9,6 +9,44 @@ import needle from 'needle';
 //     console.log(res.body);   // prints an HTML string
 // });
 
-needle.get('http://localhost:3000/greeting?name=Becca', (err, res) => {
+// needle.get('http://localhost:3000/greeting?name=Becca', (err, res) => {
+//     console.log(res.body);   // prints the body of the response message. In this case, “Hello”
+// });
+
+// needle.post(
+//     'http://localhost:3000/submit-data',
+//     { name: 'Monina' },   // can be an object or a string
+//     (err, res) => {
+//       console.log(res.body);
+//     }
+//   );
+
+//Start of the Exercise
+needle.post(
+    'http://localhost:3000/add-book',
+    { bookName: "Harry Potter and the Philosopher’s Stone", isbn: "978-0-7475-3269-9",author: "J.K Rowling", yearPublished: "1997"},   // can be an object or a string
+    (err, res) => {
+      console.log(res.body);
+    }
+  );
+
+needle.post(
+    'http://localhost:3000/add-book',
+    { bookName: 'Harry Potter and the Chamber of Secrets', isbn: "0-7475-3849-2",author: "J.K Rowling", yearPublished: "1998"},   // can be an object or a string
+    (err, res) => {
+      console.log(res.body);
+    }
+);
+
+needle.post(
+    'http://localhost:3000/add-book',
+    { bookName: 'The Little Prince', isbn: "978-0156012195",author: "Antoine Saint-Exuper", yearPublished: "1943"},   // can be an object or a string
+    (err, res) => {
+      console.log(res.body);
+    }
+);
+
+needle.get('http://localhost:3000/find-by-author?author=J.K+Rowling', (err, res) => {
     console.log(res.body);   // prints the body of the response message. In this case, “Hello”
 });
+
--- server.js
+++ server.js
@@ -1,26 +1,97 @@
 import express from 'express';
+import { appendFileSync, readFile} from 'node:fs';
 
 // instantiate the server
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 
 // this tells our app to listen for GET messages on the '/' path
 // the callback function specifies what the server will do when a message is received
-app.get('/', (req, res) => {
-    //console.log(req);
-    //console.log(res);
-    res.send('Hello!');
-});
+// app.get('/', (req, res) => {
+//     //console.log(req);
+//     //console.log(res);
+//     res.send('Hello!');
+// });
 
-app.post('/submit-data', (req, res) => {
-    res.send('Received a POST request.');
-});
+// app.post('/submit-data', (req, res) => {
+//     res.send('Received a POST request.' + req.body.name);
+// });
 
-app.get('/greeting', (req, res) => {
-  res.send('Hello ' + req.query.name);
-});
+// app.get('/greeting', (req, res) => {
+//   res.send('Hello ' + req.query.name);
+// });
 
 // this tells our server to listen to the port 3000
 // we can also pass an optional callback function to execute after the server starts
-app.listen(3000, () => { console.log('Server started at port 3000')} );
 
-//yung endpoints nagtutugma
+
+//Start of the Exercise
+app.post('/add-book', (req, res) => {
+    //save to a file 
+    var check = true;
+    var details = [];
+
+    details[0] = req.body.bookName;
+    details[1] = req.body.isbn
+    details[2] = req.body.author
+    details[3] = req.body.yearPublished
+
+    if(details.length !=4){ //incomplete fields
+        check = false;
+    }
+
+    //check if there is a non-empty string
+    if(details[0].length == 0){
+        check = false;
+    }
+
+    if(details[1].length == 0) {
+        check = false;
+    }
+
+    if(details[2].length == 0) {
+        check = false;
+    }
+    
+    if(details[3].length == 0) {
+        check = false;
+    }
+    
+    if(check){
+        //save the data on a file 
+    
+        appendFileSync('books.txt', details.join(","));
+        appendFileSync('books.txt','\n');
+
+        console.log('The "data to append" was appended to file!');
+
+        res.send({success:true});
+    } else {
+        res.send({success:false});
+    }
+ 
+});
+
+
+app.get('/find-by-author', (req, res) => {
+  readFile('books.txt', 'utf8', (err,data) =>{
+    if (err) throw err;
+
+    var lines = data.split("\n");
+    var result = [];
+
+    lines.forEach(line => {
+        var line_element = line.split(',');
+
+        if(line_element.indexOf(req.query.author) == 2){
+            console.log('Found it:' + line);
+            result.push(line);
+        }
+  });
+  res.send(result);
+});
+});
+
+app.listen(3000, () => { console.log('Server started at port 3000')} );
