3529155a52792658ea4f18b9d3174b630fe11e8b
beccajoyce <rrtorralba@up.edu.ph>
2024-03-13 17:02:22 +0800

    feat: add request.js

--- server.js
+++ server.js
@@ -6,13 +6,18 @@ const app = express();
 // this tells our app to listen for GET messages on the '/' path
 // the callback function specifies what the server will do when a message is received
 app.get('/', (req, res) => {
-  res.send('Hello!');
+    //console.log(req);
+    //console.log(res);
+    res.send('Hello!');
 });
 
 app.post('/submit-data', (req, res) => {
     res.send('Received a POST request.');
 });
 
+
 // this tells our server to listen to the port 3000
 // we can also pass an optional callback function to execute after the server starts
 app.listen(3000, () => { console.log('Server started at port 3000')} );
+
+//yung endpoints nagtutugma
